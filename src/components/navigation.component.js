c6ee7e7074a58ad97442530d09d58d586d953f3c
Vova <vova77gh@gmail.com>
2020-01-30 21:43:17 +0200

    create new components; work with tabs when user click

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -3,19 +3,30 @@ import {Component} from '../core/component'
 export class NavigationComponent extends Component {
     constructor(id) {
         super(id)
+
+        this.tabs = []
     }
 
     init() {
         this.$el.addEventListener('click', tabClickHandler.bind(this))
     }
+
+    registerTabs(tabs) {
+        this.tabs = tabs
+    }
 }
 
 function tabClickHandler(event) {
+    event.preventDefault()    
+
     if (event.target.classList.contains('tab')) {
         Array.from(this.$el.querySelectorAll('.tab')).forEach(element => {
             element.classList.remove('active')
         })
-
         event.target.classList.add('active')
+
+        const activeTab = this.tabs.find(t => t.name === event.target.dataset.name)
+        this.tabs.forEach(element => element.component.hide())
+        activeTab.component.show()
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import './css/index.css'
 import { HeaderComponent } from './components/header.component'
 import { NavigationComponent } from './components/navigation.component'
 
+import { CreateComponent } from './components/create.component'
+import { FavoriteComponent } from './components/favorite.component'
+import { PostsComponent } from './components/posts.component'
+
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
 
-console.log(navigation);
+const create = new CreateComponent('create')
+const favorite = new FavoriteComponent('favorite')
+const posts = new PostsComponent('posts')
+
+navigation.registerTabs([
+    {name: 'create', component: create},
+    {name: 'favorite', component: favorite},
+    {name: 'posts', component: posts},
+])
+
 
